a2baa96b2cb52d594a2a6d53fcd65d7d5ede9eb6
AAsriyan <aasriyan@protonmail.com>
2019-01-29 18:30:24 -0500

    Added last 5 event listeners and made anchor links stop refreshing page

--- js/index.js
+++ js/index.js
@@ -33,6 +33,132 @@ bigHeader.addEventListener('mouseout', e => {
 const textTransform = document.querySelectorAll('p');
 console.log(textTransform);
 
-textTransform[0].addEventListener('select', e => {
-	e.style.color = 'green';
+textTransform[0].addEventListener('mouseup', e => {
+	e.currentTarget.style.color = 'green';
+	e.currentTarget.style.fontSize = '20px';
+});
+
+textTransform[1].addEventListener('mouseup', e => {
+	e.currentTarget.style.color = 'green';
+	e.currentTarget.style.fontSize = '20px';
+});
+
+textTransform[2].addEventListener('mouseup', e => {
+	e.currentTarget.style.color = 'green';
+	e.currentTarget.style.fontSize = '20px';
+});
+
+textTransform[3].addEventListener('mouseup', e => {
+	e.currentTarget.style.color = 'green';
+	e.currentTarget.style.fontSize = '20px';
+});
+
+textTransform[4].addEventListener('mousedown', e => {
+	e.currentTarget.style.color = 'green';
+	e.currentTarget.style.fontSize = '20px';
+});
+
+// Changing h2 elements background color
+
+const subHeaderColor = document.querySelectorAll('h2');
+
+// Trying to figure out a forEach loop for event listeners
+
+// subHeaderColor.forEach(element => {
+// 	element.addEventListener('mouseover', e => {
+// 		e.currentTarget.style.backgroundColor = 'black';
+// 		e.currentTarget.style.color = 'white';
+// });
+
+subHeaderColor[0].addEventListener('mousemove', e => {
+	e.currentTarget.style.backgroundColor = 'black';
+	e.currentTarget.style.color = 'white';
+});
+
+subHeaderColor[1].addEventListener('mousemove', e => {
+	e.currentTarget.style.backgroundColor = 'black';
+	e.currentTarget.style.color = 'white';
+});
+
+subHeaderColor[2].addEventListener('mousemove', e => {
+	e.currentTarget.style.backgroundColor = 'black';
+	e.currentTarget.style.color = 'white';
+});
+
+subHeaderColor[3].addEventListener('mousemove', e => {
+	e.currentTarget.style.backgroundColor = 'black';
+	e.currentTarget.style.color = 'white';
+});
+
+subHeaderColor[0].addEventListener('mouseleave', e => {
+	e.currentTarget.style.backgroundColor = 'white';
+	e.currentTarget.style.color = 'black';
+});
+
+subHeaderColor[1].addEventListener('mouseleave', e => {
+	e.currentTarget.style.backgroundColor = 'white';
+	e.currentTarget.style.color = 'black';
+});
+
+subHeaderColor[2].addEventListener('mouseout', e => {
+	e.currentTarget.style.backgroundColor = 'white';
+	e.currentTarget.style.color = 'black';
+});
+
+subHeaderColor[3].addEventListener('mouseout', e => {
+	e.currentTarget.style.backgroundColor = 'white';
+	e.currentTarget.style.color = 'black';
+});
+
+const fourthHeaderText = document.querySelectorAll('h4');
+
+fourthHeaderText[0].addEventListener('click', e => {
+	e.currentTarget.textContent = 'INSERT USELESS TEXT';
+	e.currentTarget.style.fontSize = '30px';
+});
+
+fourthHeaderText[1].addEventListener('click', e => {
+	e.currentTarget.textContent = 'INSERT USELESS TEXT';
+	e.currentTarget.style.fontSize = '30px';
+});
+
+fourthHeaderText[2].addEventListener('click', e => {
+	e.currentTarget.textContent = 'INSERT USELESS TEXT';
+	e.currentTarget.style.fontSize = '30px';
+});
+
+// stopping navigation items from refreshing the page
+
+const navBar = document.querySelectorAll('nav a');
+
+navBar[0].addEventListener('click', e => {
+	e.currentTarget.style.color = 'pink';
+	e.preventDefault();
 })
+
+navBar[1].addEventListener('click', e => {
+	e.currentTarget.style.color = 'pink';
+	e.preventDefault();
+})
+
+navBar[2].addEventListener('click', e => {
+	e.currentTarget.style.color = 'pink';
+	e.preventDefault();
+})
+
+navBar[3].addEventListener('click', e => {
+	e.currentTarget.style.color = 'pink';
+	e.preventDefault();
+})
+
+// Changing input text color when selecting
+
+const inputUpdate = document.querySelectorAll('input');
+
+inputUpdate[0].addEventListener('select', e => {
+	e.currentTarget.style.color = 'green';
+});
+
+inputUpdate[1].addEventListener('select', e => {
+	e.currentTarget.style.color = 'green';
+});
